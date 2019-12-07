0563ef33f4adbc6e8b9ca6068710ec3e41cf5986
Tomek <tomekpl77@gmail.com>
2019-12-07 11:30:41 +0100

    First commit to Git

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * ... comment class...
+ *
+ * @author tomekpl77@gmail.com
+ * @since 07.12.2019
+ */
+final class Main {
+    public static void main(String[] args) {
+        System.out.println("blablabla");
+    }
+}
