04d9a6628319228d3e26a7aa85a316601fadb17f
Szymek582 <szymon.olucha@gmail.com>
2019-12-07 13:59:15 +0100

    6

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,9 @@ public final class Main {
     public static void main(String[] args) {
         System.out.println("opopopTESTfewfwferwrwerwerwerwrewerwe");
         System.out.println("ZZZZZZZZ122222");
-        int a;
-        int b;
+        int a=5;
+        int b=5;
+        int c=a+b;
 
 
     }
