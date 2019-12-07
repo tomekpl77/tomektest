6d8a6c19e4d9436d0b5e1732a2b92f8bc39013e3
Szymek582 <szymon.olucha@gmail.com>
2019-12-07 11:53:41 +0100

    New sout

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,7 @@
 final class Main {
     public static void main(String[] args) {
         System.out.println("blablabla");
+        System.out.println(" ");
+        System.out.println("OK ");
     }
 }
