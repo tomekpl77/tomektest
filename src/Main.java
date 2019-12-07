0cc25d22105cf16752d0e26daf20549acf6b14d2
Szymek582 <szymon.olucha@gmail.com>
2019-12-07 15:06:48 +0100

    Name

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args) {
-
-        String [] abc = new String[]{"A","B","C","D"};
+        String [] abc = new String[]{"A","B","C","D", "E"};
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Provide name");
+        String name1= scanner.nextLine();
+        System.out.println(name1);
 
         Human human = new Human(29, 85, 192, "Tomek", "Github");
         System.out.println(human);
