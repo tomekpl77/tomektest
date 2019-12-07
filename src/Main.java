a38df772460ded86e966d612916a3be679997a7e
Tomek <tomekpl77@gmail.com>
2019-12-07 14:35:17 +0100

    merged

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main{
     public static void main(String[] args) {
 
         Human human = new Human(29, 85, 192, "Tomek", "Github");
-        System.out.println(human.toString());
+        System.out.println(human);
     }
 }
